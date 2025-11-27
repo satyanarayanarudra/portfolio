370d0750cf82c9c10144b37fac29f89464c5645f
satyanarayanarudra <satyanarayanarudra567@gmail.com>
2025-11-27 11:54:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/hema567portfolio/",   // ✅ REQUIRED FOR GITHUB PAGES
+  base: "/portfolio/",   // ✅ REQUIRED FOR GITHUB PAGES
 
   server: {
     host: "::",
